8d19e773b67b99b597fc81ab80388140b79e3935
WissemJderi <asqr43641@gmail.com>
2026-06-25 00:23:29 +0100

    add post and get bookmarks

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class BookmarkIn(BaseModel):
+    title: str
+    url: HttpUrl
+    tags: list[str] = []
+
+
+class BookmarkOut(BookmarkIn):
+    id: UUID
+    read: bool = False
+
+
+bookmarks: dict[str, "BookmarkOut"] = {}
+
+
+@app.post("/bookmarks")
+async def create_bookmark(bookmark: BookmarkIn):
+    bookmark_id = uuid4()
+    bookmark_out = BookmarkOut(
+        id=bookmark_id,
+        title=bookmark.title,
+        url=bookmark.url,
+        tags=bookmark.tags,
+        read=False,
+    )
+
+    bookmarks[str(bookmark_id)] = bookmark_out
+    return bookmark_out
+
+
+@app.get("/bookmarks")
+async def get_bookmarks():
+    return bookmarks
